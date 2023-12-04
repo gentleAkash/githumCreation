758286aef4f3a9da2fbfe35cb8bc6ca52a764c15
gentleAkash <palakash70114@gmail.com>
2023-12-04 23:55:39 +0530

    kaho kareja

--- src/githumCreation/com/excutegithub.java
+++ src/githumCreation/com/excutegithub.java
@@ -3,6 +3,7 @@ package githumCreation.com;
 public class excutegithub {
 	public static void main(String[] args) {
 		 System.out.println("he hamar jaan");
+		 System.out.println("tum kaha the kya kar rahe the");
 	}
 
 }
